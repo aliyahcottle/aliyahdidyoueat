37912ae10aff3a073ade5911ef4961ab8a9e4260
Aliyah Cottle <aliyahrcottle@gmail.com>
2020-11-24 14:12:59 -0500

    structure resturantData file

--- src/resturants.js
+++ src/resturants.js
@@ -1,16 +1,128 @@
 export const resturantData = [
-    {"id": 1, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Cabanos", "category": "meats", "website": "http://cabanos.ca", "bg-pic": "" },
-    {"id": 2, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Chica's Nashville Hot Chicken", "category": "meats", "website": "https://www.chicaschicken.net/order/", "bg-pic": "" },
-    {"id": 3, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Kansas King", "category": "meats", "website": "https://www.kansasking.com/menus", "bg-pic": "" },
-    {"id": 4, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "XXI Chophouse", "category": "meats", "website": "https://www.xxichophouse.com/dinner", "bg-pic": "" },
-    {"id": 5, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Wow! Wing House", "category": "meats", "website": "http://www.wowwinghouse.com/morningside-milner/", "bg-pic": "" },
-    {"id": 6, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Top Gun Burger", "category": "meats", "website": "http://www.topgunburgerto.com/menu/", "bg-pic": "" },
-    {"id": 7, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "The Captain's Boil", "category": "seafood", "website": "https://thecaptainsboil.com/locations/", "bg-pic": "" },
-    {"id": 8, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Goodfellas Wood Stove Pizza", "category": "pizza", "website": "", "bg-pic": "" },
-    {"id": 9, "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", "name": "Papa Giuseppes", "category": "pizza", "website": "", "bg-pic": "" },
-    {"id": 10,"description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  "name": "KyKy Kookies", "category": "dessert", "website": "https://www.kykykookies.com/", "bg-pic": "" },
-    {"id": 11,"description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  "name": "Holy Shakes", "category": "food-truck", "website": "https://416-food-truck-company.square.site/", "bg-pic": "" },
-    {"id": 12,"description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  "name": "August 8", "category": "seafood", "website": "http://august8.ca/", "bg-pic": "" },
-    {"id": 13,"description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  "name": "Kaka", "category": "seafood", "website": "http://kakaallyoucaneat.ca/", "bg-pic": "" },
-    {"id": 14,"description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  "name": "Barsa", "category": "tapas", "website": "https://barsataberna.com/", "bg-pic": "" }   
+    {
+        "id": 1, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Cabanos", 
+        "category": "meats", 
+        "website": "http://cabanos.ca", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 2, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Chica's Nashville Hot Chicken", 
+        "category": "meats", 
+        "website": "https://www.chicaschicken.net/order/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 3, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Kansas King", 
+        "category": "meats", 
+        "website": "https://www.kansasking.com/menus", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {   
+        "id": 4, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "XXI Chophouse", 
+        "category": "meats", 
+        "website": "https://www.xxichophouse.com/dinner", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 5, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Wow! Wing House", 
+        "category": "meats", 
+        "website": "http://www.wowwinghouse.com/morningside-milner/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 6, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Top Gun Burger", 
+        "category": "meats", 
+        "website": "http://www.topgunburgerto.com/menu/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 7, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "The Captain's Boil", 
+        "category": "seafood", 
+        "website": "https://thecaptainsboil.com/locations/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 8, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Goodfellas Wood Stove Pizza", 
+        "category": "pizza", 
+        "website": "", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 9, 
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it", 
+        "name": "Papa Giuseppes", 
+        "category": "pizza", 
+        "website": "", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 10,
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  
+        "name": "KyKy Kookies", 
+        "category": "dessert", 
+        "website": "https://www.kykykookies.com/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 11,
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  
+        "name": "Holy Shakes", 
+        "category": "food-truck", 
+        "website": "https://416-food-truck-company.square.site/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 12,
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  
+        "name": "August 8", 
+        "category": "seafood", 
+        "website": "http://august8.ca/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 13,
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  
+        "name": "Kaka", 
+        "category": "seafood", 
+        "website": "http://kakaallyoucaneat.ca/", 
+        "bg-pic": "", 
+        "phone": "" 
+    },
+    {
+        "id": 14,
+        "description": "Aliyah's take: here I put the information that I would want my date or friends to hear about this place and my fav things about it",  
+        "name": "Barsa", 
+        "category": "tapas", 
+        "website": "https://barsataberna.com/", 
+        "bg-pic": "", 
+        "phone": "" 
+    }   
 ]; 
